8378924b269969e2c0195b13aa2e0109ee5cd8eb
LukeMoran <2259458m@student.gla.ac.uk>
2023-03-15 16:57:50 +0000

    Actually fixed file paths for pages deploy

--- src/App.js
+++ src/App.js
@@ -101,14 +101,14 @@ function DrumPad({ audioclip, hotkey, padPressed}) {
 const hotkeys = ["Q", "W", "E", "A", "S", "D", "Z", "X", "C"];
 const tracks = [
   "./audio/Heater-1.mp3",
-  "drum-app/audio/Heater-2.mp3",
-  "drum-app/audio/Heater-3.mp3",
-  "drum-app/audio/Heater-4_1.mp3",
-  "drum-app/audio/Heater-6.mp3",
-  "drum-app/audio/Cev_H2.mp3",
-  "drum-app/audio/Kick_n_Hat.mp3",
-  "drum-app/audio/RP4_KICK_1.mp3",
-  "drum-app/audio/Dsc_Oh.mp3"
+  "./audio/Heater-2.mp3",
+  "./audio/Heater-3.mp3",
+  "./audio/Heater-4_1.mp3",
+  "./audio/Heater-6.mp3",
+  "./audio/Cev_H2.mp3",
+  "./audio/Kick_n_Hat.mp3",
+  "./audio/RP4_KICK_1.mp3",
+  "./audio/Dsc_Oh.mp3"
 ]
 
 
